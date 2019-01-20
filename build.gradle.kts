0fb9d786e03cf810a3ecbf12561a8163f6ba44ed
Roman Mityukov <roman.mityukov@actonica.ru>
2019-01-20 07:09:35 +0700

    update agp to 3.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
 
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.3.0-rc03")
+        classpath("com.android.tools.build:gradle:3.3.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.11")
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
     }
